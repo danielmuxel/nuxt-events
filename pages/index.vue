863762ae4aa5583310221b4008fcf4eb7e801fd0
Daniel Muxel <daniel.muxel@gmail.com>
2021-05-05 14:33:07 +0200

    api calls with axios

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,19 @@
 
 <script>
 export default {
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return { events: response.data }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events as this time. Please try again',
+        })
+      })
+  },
   head() {
     return {
       title: 'Event Listing',
